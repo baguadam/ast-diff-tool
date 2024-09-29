63da103174d44ba4f59a7126a381b2d3e8a6356b
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-09-29 19:51:10 +0200

    Create IFileDialogService and FileDialogService skeleton

---
+++ ASTDiffTool.Services/FileDialogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ASTDiffTool.Services
+{
+    public class FileDialogService : IFileDialogService
+    {
+        public string OpenFile(string filter)
+        {
+            return null;
+        }
+    }
+}
