d4ce0f883703a477f8002a5ee62b78129b150126
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-03 15:46:17 +0200

    Implement generic navigation service

---
+++ ASTDiffTool.ViewModels/Services/INavigateService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.ViewModels.Services
+{
+    public interface INavigateService
+    {
+        ViewModelBase CurrentViewModel { get; }
+        void NavigateTo<TViewModel>() where TViewModel : ViewModelBase;
+    }
+}
---
+++ ASTDiffTool.ViewModels/Services/NavigationService.cs
@@ -0,0 +1,27 @@
+﻿using ASTDiffTool.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.ViewModels.Services
+{
+    public class NavigationService : INavigateService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public NavigationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ViewModelBase CurrentViewModel { get; private set; }
+
+        public void NavigateTo<TViewModel>() where TViewModel : ViewModelBase
+        {
+            CurrentViewModel = _serviceProvider.GetRequiredService<TViewModel>();
+        }
+    }
+}
