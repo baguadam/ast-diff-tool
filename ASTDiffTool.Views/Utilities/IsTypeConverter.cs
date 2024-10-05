8f0d32b336d2ded469409e8728f49005a0fee8db
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-05 17:25:05 +0200

    Create IsTypeConverter

---
+++ ASTDiffTool.Views/Utilities/IsTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ASTDiffTool.Views.Utilities
+{
+    public class IsTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value?.GetType() == parameter as Type;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
