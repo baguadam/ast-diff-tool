1f154d59c2b23111b1717f5c1af07f3b8aa3df5b
Bagu Adam <bau9bp@bosch.com>
2024-10-09 10:28:43 +0200

    Create the skeleton of text displayer and use it in ASTPage.xaml

--- ASTDiffTool.ViewModels/ASTPageViewModel.cs
+++ ASTDiffTool.ViewModels/ASTPageViewModel.cs
@@ -2,15 +2,26 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASTDiffTool.ViewModels
 {
-    public partial class ASTPageViewModel(Project project) : ViewModelBase
+    public partial class ASTPageViewModel : ViewModelBase
     {
-        private readonly Project _project = project;
+        private readonly Project _project;
+
+        public ObservableCollection<LineViewModel> FirstStandardLines { get; set; } = new ObservableCollection<LineViewModel>();
+        public ObservableCollection<LineViewModel> SecondStandardLines { get; set; } = new ObservableCollection<LineViewModel>();
+
+        public ASTPageViewModel(Project project)
+        {
+            _project = project;
+
+            FillLinesWithDummyData();
+        }
 
         public int NumberOfDifferences
         {
@@ -19,6 +30,25 @@ namespace ASTDiffTool.ViewModels
             {
                 _project.NumberOfDifferences = value;
                 OnPropertyChanged(nameof(NumberOfDifferences));
+            }
+        }
+
+        private void FillLinesWithDummyData()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var firstStandardLineViewModel = new LineViewModel { Line = $"Line {i + 1} in File 1" };
+                var secondStandardLineViewModel = new LineViewModel { Line = $"Line {i + 1} in File 2" };
+
+                // Highlight every 10th line
+                if (i % 10 == 0)
+                {
+                    firstStandardLineViewModel.State = LineState.Highlighted;
+                    secondStandardLineViewModel.State = LineState.Highlighted;
+                }
+
+                FirstStandardLines.Add(firstStandardLineViewModel);
+                SecondStandardLines.Add(secondStandardLineViewModel);
             }
         }
     }
