7ca713e7316c8674908dc6c73fed4129d0b0c58f
Bagu Adam <bau9bp@bosch.com>
2024-11-06 14:23:20 +0100

    create Node and Edge model, add DatabaseContext class

---
+++ ASTDiffTool.Models/Edge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.Models
+{
+    public class Edge
+    {
+        public string ChildId { get; set; }
+        public string ParentId { get; set; }
+
+        // Navigation properties to the related Node entities
+        public Node ChildNode { get; set; }
+        public Node ParentNode { get; set; }
+    }
+}
---
+++ ASTDiffTool.Models/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.Models
+{
+    public class Node
+    {
+        public string Id { get; set; }
+        public int Type { get; set; }
+        public string Kind { get; set; }
+        public string Usr { get; set; }
+        public string Path { get; set; }
+        public int DifferenceType { get; set; }
+        public int AST { get; set; }
+        public bool IsHighestLevelNode { get; set; } = false;
+        public string Comment { get; set; }
+
+        // Navigation property for related Edge entities where this Node is a parent
+        public ICollection<Edge> ParentEdges { get; set; }
+
+        // Navigation property for related Edge entities where this Node is a child
+        public ICollection<Edge> ChildEdges { get; set; }
+    }
+}
