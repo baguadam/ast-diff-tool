58fc2516b0370a14fb5288bb7e7d9837a2f8d8dc
Bagu Adam <bau9bp@bosch.com>
2024-11-06 14:32:32 +0100

    create DbSets in DatabaseContext + implement OnModelCreating

--- ASTDiffTool.Services/DatabaseContext.cs
+++ ASTDiffTool.Services/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ASTDiffTool.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace ASTDiffTool.Services
 {
-    internal class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        public DbSet<Node> Nodes {  get; set; }
+        public DbSet<Edge> Edges { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the primary key for the Node entity
+            _ = modelBuilder.Entity<Node>()
+                .HasKey(n => n.Id);
+
+            // Configure the composite primary key for the Edge entity
+            _= modelBuilder.Entity<Edge>()
+                .HasKey(e => new { e.ChildId, e.ParentId });
+
+            // Configure the foreign key relationships for the Edge entity
+            _ = modelBuilder.Entity<Edge>()
+                .HasOne(e => e.ParentNode)
+                .WithMany(n => n.ParentEdges)
+                .HasForeignKey(e => e.ParentId)
+                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
+
+            _ = modelBuilder.Entity<Edge>()
+                .HasOne(e => e.ChildNode)
+                .WithMany(n => n.ChildEdges)
+                .HasForeignKey(e => e.ChildId)
+                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
+        }
     }
 }
