983b120559460e272dfe6d183b7a2fafac061b10
baguadam <m3wdo5@inf.elte.hu>
2024-11-19 15:19:52 +0100

    Implement FileService with correcponding interface

---
+++ ASTDiffTool.Services/FileService.cs
@@ -0,0 +1,51 @@
+﻿using ASTDiffTool.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.Services
+{
+    public class FileService : IFileService
+    {
+        public string ReadFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found: {filePath}");
+            }
+
+            return File.ReadAllText(filePath);
+        }
+
+        public void WriteFile(string filePath, string content)
+        {
+            File.WriteAllText(filePath, content);
+        }
+
+        public string CreateTemporaryFile(string directory, string fileName)
+        {
+            EnsureDirectoryExists(directory);
+            string filePath = Path.Combine(directory, fileName);
+            File.Create(filePath).Dispose();
+            return filePath;
+        }
+
+        public void EnsureDirectoryExists(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        public void DeleteFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
---
+++ ASTDiffTool.Services/Interfaces/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.Services.Interfaces
+{
+    public interface IFileService
+    {
+        string ReadFile(string filePath);
+        void WriteFile(string filePath, string content);
+        string CreateTemporaryFile(string directory, string fileName);
+        void EnsureDirectoryExists(string directoryPath);
+        void DeleteFile(string filePath);
+    }
+}
