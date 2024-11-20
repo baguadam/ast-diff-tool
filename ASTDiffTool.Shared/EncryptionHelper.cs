23622a0156376f2beea38bc494ca3041bac57cdf
baguadam <m3wdo5@inf.elte.hu>
2024-11-20 15:45:36 +0100

    Store encryption key and iv in environment variables, get the values from there

---
+++ ASTDiffTool.Shared/EncryptionHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace ASTDiffTool.Shared
+{
+    public class EncryptionHelper
+    {
+        private static readonly byte[] Key = Convert.FromBase64String(Environment.GetEnvironmentVariable("ENCRYPTION_KEY") ?? throw new InvalidOperationException("ENCRYPTION_KEY not set."));
+        private static readonly byte[] IV = Convert.FromBase64String(Environment.GetEnvironmentVariable("ENCRYPTION_IV") ?? throw new InvalidOperationException("ENCRYPTION_IV not set."));
+
+        public static string Encrypt(string plainText)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                var plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+                var encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                var encryptedBytes = Convert.FromBase64String(encryptedText);
+
+                var decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+    }
+}
