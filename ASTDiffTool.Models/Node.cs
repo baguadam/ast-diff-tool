062753e2705373d81a99a2edfb73123423ac2245
baguadam <m3wdo5@inf.elte.hu>
2024-11-20 21:58:31 +0100

    Create Node model

---
+++ ASTDiffTool.Models/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.Models
+{
+    public class Node
+    {
+        public string EnhancedKey { get; set; }
+        public string Type { get; set; }
+        public string DifferenceType { get; set; }
+        public int TopologicalOrder {  get; set; }
+        public string Path {  get; set; }
+        public int LineNumber {  get; set; }
+        public int ColumnNumber { get; set; }
+        public bool IsHighLevel {  get; set; }
+        public Node? Parent { get; set; }
+        public List<Node> Children { get; set; } = [];
+
+        public override string ToString()
+        {
+            return $"{Type} - {EnhancedKey} - {Path} {LineNumber}:{ColumnNumber}";
+        }
+    }
+}
