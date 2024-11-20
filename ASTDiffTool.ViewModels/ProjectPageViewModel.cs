bd13b12470154ff84ab21ff4bf16da3c6232f674
baguadam <m3wdo5@inf.elte.hu>
2024-11-20 16:42:44 +0100

    Create basic ProjectPage.xaml.cs, Implement basic viewmodel of the project page with a simple query

--- ASTDiffTool.ViewModels/ProjectPageViewModel.cs
+++ ASTDiffTool.ViewModels/ProjectPageViewModel.cs
@@ -1,6 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using ASTDiffTool.Services.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +12,44 @@ namespace ASTDiffTool.ViewModels
 {
     public partial class ProjectPageViewModel : ViewModelBase
     {
-        private string _title = "Project Page View";
+        private readonly INeo4jService _neo4jService;
 
-        public string Title { get { return _title; } }
+        public ProjectPageViewModel(INeo4jService neo4jService)
+        {
+            _neo4jService = neo4jService;
+        }
+
+        #region Properties
+        private int _totalNodeCount;
+
+        public int TotalNodeCount
+        {
+            get => _totalNodeCount;
+            set
+            {
+                if (_totalNodeCount != value)
+                {
+                    _totalNodeCount = value;
+                    OnPropertyChanged(nameof(TotalNodeCount));
+                }
+            }
+
+        }
+        #endregion
+
+        #region Commands
+        [RelayCommand]
+        public async Task LoadDatabaseInfoAsync()
+        {
+            try
+            {
+                TotalNodeCount = await _neo4jService.GetNodeCountAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error loading database info: {ex.Message}");
+            }
+        }
+        #endregion
     }
 }
