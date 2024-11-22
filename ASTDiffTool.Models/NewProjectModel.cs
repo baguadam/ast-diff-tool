851b0e12b28ae632bb14ba5b867198ab3b799b8c
baguadam <m3wdo5@inf.elte.hu>
2024-11-22 20:29:13 +0100

    Comments for the model classes

--- ASTDiffTool.Models/NewProjectModel.cs
+++ ASTDiffTool.Models/NewProjectModel.cs
@@ -6,15 +6,44 @@ using System.Threading.Tasks;
 
 namespace ASTDiffTool.Models
 {
+    /// <summary>
+    /// Represents the model of a new project.
+    /// </summary>
     public class NewProjectModel
     {
+        /// <summary>
+        /// The path to the compilation database.
+        /// </summary>
         public string CompilationDatabasePath { get; set; }
+
+        /// <summary>
+        /// The path to the main file of the project.
+        /// </summary>
         public string MainFilePath { get; set; }
+
+        /// <summary>
+        /// The name of the new project.
+        /// </summary>
         public string ProjectName { get; set; }
+
+        /// <summary>
+        /// The first standard that is selected for the dump tool.
+        /// </summary>
         public string FirstSelectedStandard { get; set; }
+
+        /// <summary>
+        /// The second standard that is selected for the dump tool.
+        /// </summary>
         public string SecondSelectedStandard { get; set; }
+
+        /// <summary>
+        /// The path to the project folder that contains the dumped ASTs, logs.
+        /// </summary>
         public string ProjectResultPath {  get; set; }
 
+        /// <summary>
+        /// Indicates whether everything is set.
+        /// </summary>
         public bool IsComplete =>
             !string.IsNullOrWhiteSpace(CompilationDatabasePath) &&
             !string.IsNullOrWhiteSpace(MainFilePath) &&
