eda451a35c842b3cf75f89ba210268f6dca19735
baguadam <m3wdo5@inf.elte.hu>
2024-11-22 15:59:59 +0100

    docker-compose.yml for testing, update TestInitializer.cs

---
+++ ASTDiffTool.Tests/Neo4jServiceIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using ASTDiffTool.Services;
+using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTDiffTool.Tests
+{
+    public class Neo4jServiceIntegrationTests : IClassFixture<Neo4jTestFixture>
+    {
+        private readonly IDriver _driver;
+
+        public Neo4jServiceIntegrationTests()
+        {
+            _driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "testpassword"));
+        }
+
+        [Fact]
+        public async Task GetNodeCountAsync_ReturnsCorrectCount()
+        {
+            // seed the database
+            var seedQuery = @"
+            CREATE (n:Node {name: 'Test Node', diffType: 'SomeType'}),
+                   (m:Node {name: 'Another Node', diffType: 'AnotherType'})";
+
+            using var session = _driver.AsyncSession();
+            await session.RunAsync(seedQuery);
+
+            var service = new Neo4jService(_driver);
+
+            // call service method
+            var count = await service.GetNodeCountAsync();
+
+            // assert
+            Assert.Equal(2, count);
+        }
+    }
+}
