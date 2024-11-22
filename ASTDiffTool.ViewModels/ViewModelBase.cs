723f2a64f1d669661d55f6d20fb15894eacf8bf8
baguadam <m3wdo5@inf.elte.hu>
2024-11-22 19:38:42 +0100

    Documentation comments for TreeDisplayPageViewModel and ViewModelBase

--- ASTDiffTool.ViewModels/ViewModelBase.cs
+++ ASTDiffTool.ViewModels/ViewModelBase.cs
@@ -8,9 +8,20 @@ using System.Threading.Tasks;
 
 namespace ASTDiffTool.ViewModels
 {
+    /// <summary>
+    /// Base class of ViewModels, implements the INotifyPropertyChanged
+    /// </summary>
     public class ViewModelBase : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Raised whenever property value changes
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Notifies subscribers when the value changes
+        /// </summary>
+        /// <param name="propertyName">The name of the changed property</param>
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
